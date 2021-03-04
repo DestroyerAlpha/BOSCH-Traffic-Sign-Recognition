c567ae6073bd3135834a3b7805646207874b85bc
IamODJ <omkarj1422001@gmail.com>
2021-03-04 13:14:05 +0530

    React template added using create-react-app package

---
+++ bosch-frontend/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <div>Hello</div>
+    );
+  }
+}
+
+export default App;
